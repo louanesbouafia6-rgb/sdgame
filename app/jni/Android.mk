97982873c7494e7600e0e1b4b65382c6206b4cf4
louanesbouafia6-rgb <louanesbouafia6@gmail.com>
2025-11-17 04:03:07 +0100

    Fix SDL2 inclusion in Android.mk

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,1 +1,16 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+# SDL2 module
+include $(CLEAR_VARS)
+LOCAL_MODULE := SDL2
+LOCAL_SRC_FILES := $(LOCAL_PATH)/SDL/Android.mk
+include $(LOCAL_PATH)/SDL/Android.mk
+
+# Your game
+include $(CLEAR_VARS)
+LOCAL_MODULE := main
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/SDL/include
+LOCAL_SRC_FILES := src/main.cpp
+LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_LDLIBS := -lGLESv2 -llog -landroid
+include $(BUILD_SHARED_LIBRARY)
