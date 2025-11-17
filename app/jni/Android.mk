691fc59d12cdff5289694887b2b4c78359e424ed
louanesbouafia6-rgb <louanesbouafia6@gmail.com>
2025-11-17 12:28:49 +0100

    Fix: Properly include SDL Android.mk

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,16 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-# SDL2 module
-include $(CLEAR_VARS)
-LOCAL_MODULE := SDL2
-LOCAL_SRC_FILES := $(LOCAL_PATH)/SDL/Android.mk
+# Include SDL2's Android.mk (this will define the SDL2 module)
 include $(LOCAL_PATH)/SDL/Android.mk
 
-# Your game
+# Now build your game
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := main
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/SDL/include
 LOCAL_SRC_FILES := src/main.cpp
 LOCAL_SHARED_LIBRARIES := SDL2
 LOCAL_LDLIBS := -lGLESv2 -llog -landroid
+
 include $(BUILD_SHARED_LIBRARY)
